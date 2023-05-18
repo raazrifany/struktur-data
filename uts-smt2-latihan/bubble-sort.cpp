75176be01d347993a2e1ac4c7ab725e32fe8252e
V <116948898+raazrifany@users.noreply.github.com>
2023-05-18 12:38:39 +0700

    Create bubble-sort.cpp

---
+++ uts-smt2-latihan/bubble-sort.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+//bubble sort ascending
+main(){
+	int banyak_data, i, data_ke[5], j, temp;
+	
+	system("cls");
+	printf("Banyak data : "); scanf("%d",&banyak_data);
+	for(i=0; i<banyak_data; i++){
+		printf("Data ke-%d : ",i+1); scanf("%d",&data_ke[i]);
+	}
+	
+	for(i=0; i<banyak_data-1; i++){
+		for(j=0; j<banyak_data-1-i; j++){
+			if(data_ke[j+1]<data_ke[j]){
+				temp=data_ke[j];
+				data_ke[j]=data_ke[j+1];
+				data_ke[j+1]=temp;
+			}
+		}
+	}
+	
+	printf("\nData array terurut\n");
+	for(i=0; i<banyak_data; i++){
+		printf("%3i",data_ke[i]);
+	}
+	
+	getch();
+}
